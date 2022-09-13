ea1853ee2cd56376a025ffdff15a538f189a9b94
Guilherme Cidade <guirevcidade1@gmail.com>
2022-09-12 21:49:36 -0300

    first commit

---
+++ src/main/java/com/transport/transquest/TransquestApplication.java
@@ -0,0 +1,13 @@
+package com.transport.transquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransquestApplication.class, args);
+	}
+
+}
